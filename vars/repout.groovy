6f19919320dc82f90a3cb8bf5ced9db55d4517bb
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-17 17:18:03 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -1,7 +1,7 @@
 def call(def giturl, def branch){
     echo "Cloning started"
      checkout(
-                [$class: 'GitSCMCheckout', branches: [[name: '*/branch']],
+                [$class: 'GitSCM', branches: [[name: '*/branch']],
                  userRemoteConfigs: [[credentialsId: 'mine', url: 'giturl']]],
      )
 }
