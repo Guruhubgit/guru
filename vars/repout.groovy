72ec55e7171cab2dc3a4cd8c1f4402aea970a0fb
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-20 23:41:42 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -25,7 +25,7 @@ for (repo in gitNameArray) {
     }
     else{
      writer_date = sh (returnStdout: true, script:"""
-        curl -s https://api.github.com/repos/Guruhubgit/liauibasedemo/branches/$repo|jq '.commit.commit.author.date'
+        curl -s https://api.github.com/repos/Guruhubgit/liauibasedemo/branches/$repo jq '.commit.commit.author.date'
         """)
         echo "$writer_date"
     }
