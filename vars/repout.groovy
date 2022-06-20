67c660691c7e78fa6f93d013a7eee4535e613da4
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-20 19:27:25 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -18,7 +18,7 @@ def call(def giturl, def branch, def cred){
    echo "$repo"  
     
      last_updated_date = sh (returnStdout: true, script:"""  
-        for (i in $repo); do
+        for ($repo in $repo_branch); do
         echo "done"
         """)
 }
