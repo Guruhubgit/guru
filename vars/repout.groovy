89a4c569ae12108c6f0279ecd667d1ae813a7df2
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-20 19:01:40 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -18,7 +18,7 @@ def call(def giturl, def branch, def cred){
    echo "$repo"  
     
      last_updated_date = sh (returnStdout: true, script:"""
-        for (branch in $repo) do
+        for (branch in $repo); do
         curl -s https://api.github.com/repos/Guruhubgit/guru/branches/$repo | jq '.commit.commit.author.date'
         done
         """)
