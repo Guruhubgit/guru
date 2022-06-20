98fb7fa8aa4bfa02f1f9e4fc7f984865e337e489
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-20 22:26:20 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -25,7 +25,7 @@ for (repo in gitNameArray) {
     }
     else{
      repo_branch = sh (returnStdout: true, script:"""
-        curl -s -u $user_name:$password https://api.github.com/repos/Guruhubgit/liauibasedemo/branches/$repo | jq '.commit.commit.author.date'
+        curl -s https://api.github.com/repos/Guruhubgit/liauibasedemo/branches/$repo | jq '.commit.commit.author.date'
         """)
     }
 }
