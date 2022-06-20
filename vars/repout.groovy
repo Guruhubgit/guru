3a960990f69ff3b239556d3fc4f9e431be634150
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-20 12:21:43 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -4,7 +4,7 @@ def call(def giturl, def branch, def cred){
                 [$class: 'GitSCM', branches: [[name: "*/$branch"]],
                  userRemoteConfigs: [[credentialsId: "$cred", url: "$giturl"]]],
      )
+}
     echo "looking for branches"
    
     repo_branch =  sh 'curl -s https://api.github.com/repos/Guruhubgit/guru/branches | jq '.[].name''
-}
