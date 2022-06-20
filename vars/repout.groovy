ff53f2e6d4fc115e2038f35d83ba446e8bbb2a76
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-20 12:58:28 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -9,6 +9,7 @@ def call(def giturl, def branch, def cred){
     
     repo_branch = sh (script:"""
          pwd
+         curl -s https://api.github.com/repos/Guruhubgit/guru/branches | jq '.[].name'
         """)
 
 }
