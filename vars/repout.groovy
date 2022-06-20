e01abc61422fba502c3c4fb8a553bf7d5d233d85
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-20 16:34:21 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -7,7 +7,7 @@ def call(def giturl, def branch, def cred){
     echo "looking for branches"
     
     repo_branch = sh (script:"""
-         list_of_branches = curl -s https://api.github.com/repos/Guruhubgit/guru/branches | jq '.[].name'
+         list_of_branches = ${curl -s https://api.github.com/repos/Guruhubgit/guru/branches | jq '.[].name'}
          for branch in $list_of_branches; do 
          branch_name = (echo ${branch} | cut -d'"' -f 2)     
         """)
