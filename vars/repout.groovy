f8265c2296fef57243d48481890aec2f95221f02
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-21 13:53:32 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -16,21 +16,7 @@ def call(def giturl, def branch, def cred){
         echo "$repo_branch"
         """)
    echo "$repo"  
-    
-    class Example { 
-   static void main(String[] args) { 
-      String sample = "Hello world"; 
-      println(sample[4]); // Print the 5 character in the string
-		
-      //Print the 1st character in the string starting from the back 
-      println(sample[-1]); 
-      println(sample[1..2]);//Prints a string starting from Index 1 to 2 
-      println(sample[4..2]);//Prints a string starting from Index 4 back to 2 
-      
-   } 
-}
-    
-    String[] array= "$repo".split('/n');
+        String[] array= "$repo_branch".split('"');
                     for(counter in array)
                     {
                         echo "$counter"
