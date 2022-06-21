a95a3be9d6901babb23ecf5163950c97aecebc85
Guruhubgit <90470020+Guruhubgit@users.noreply.github.com>
2022-06-21 12:08:13 +0530

    Update repout.groovy

--- vars/repout.groovy
+++ vars/repout.groovy
@@ -19,14 +19,15 @@ def call(def giturl, def branch, def cred){
 
 def gitNameArray = ["$repo"]
 for (repo in gitNameArray) {
-    if($repo -eq $repo) {
-        repo_branch = sh (returnStdout: true, script:"""
+    if($repo -eq main); then {
+        test = sh (returnStdout: true, script:"""
         curl -s -u $user_name:$password https://api.github.com/repos/Guruhubgit/liauibasedemo/branches/main | jq '.commit.commit.author.date'
         """)
     }
     else{
     echo "go next"
          }
+    fi
 }     
 
 }
